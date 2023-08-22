5cd9e41a8f9bd5cdae313b1a14c043d20846d629
k-kares <karlo.kares7@gmail.com>
2023-08-22 08:33:57 +0200

    Dodo class library sa metodom za čitanje podataka

---
+++ Utilities/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace Utilities
+{
+    public class FileUtilities
+    {
+        private const char SEPERATOR = '|';
+        public static List<string> LoadFileData(string PATH)
+        {
+            List<string> fileData = new List<string>();
+            if (!File.Exists(PATH))
+            {
+                return fileData;
+            }
+
+            string[] lines = File.ReadAllLines(PATH);
+
+            foreach (string line in lines)
+            {
+                string[] details = line.Split(SEPERATOR);
+                for (int i = 0; i < details.Length; i++)
+                {
+                    fileData.Add(details[i]);
+                }
+            }
+
+            return fileData;
+        }
+    }
+}
