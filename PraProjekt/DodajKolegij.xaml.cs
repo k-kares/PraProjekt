7b976f0a0021ec7ac60e81b0bb82a1ad4d4c6cfa
LovroPavlov <lovro.pavlov@racunarstvo.hr>
2023-09-01 01:06:14 +0200

    Napravio prozor za dodavanje novih kolegija

--- PraProjekt/DodajKolegij.xaml.cs
+++ PraProjekt/DodajKolegij.xaml.cs
@@ -12,17 +12,35 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Utilities;
+using System.IO;
 
 namespace PraProjekt
 {
     /// <summary>
     /// Interaction logic for DodajKolegij.xaml
     /// </summary>
-    public partial class DodajKolegij : UserControl
+    public partial class DodajKolegij : Window
     {
-        public DodajKolegij()
+        MainWindow MainW;
+        private User OvajUser;
+        public DodajKolegij(MainWindow mw)
         {
+            MainW = mw;
             InitializeComponent();
+
+            OvajUser = mw.OvajUser;
+            tbPredavačKolegija.Text = OvajUser.Name;
+        }
+
+        private void btnDodaj_Click(object sender, RoutedEventArgs e)
+        {
+            String kolegij = Environment.NewLine + $"{tbNazivKolegija.Text}|{tbPredavačKolegija.Text}";
+
+            /*ne znam kako pristupiti pathu bez da pretvorim mw.kolegiji_path u public property umjesto const
+            string kolegijPath = mw.Kolegiji_Path;*/
+            File.AppendAllText("Podaci/kolegiji.txt", kolegij);
+            this.Close();
         }
     }
 }
