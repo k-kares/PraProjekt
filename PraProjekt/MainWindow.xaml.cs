61e11576b39f4e9fef1b8b88decaab4b83838d7d
k-kares <karlo.kares7@gmail.com>
2023-08-31 09:53:13 +0200

    Poceo logiku za obavijesti i predmete

--- PraProjekt/MainWindow.xaml.cs
+++ PraProjekt/MainWindow.xaml.cs
@@ -24,12 +24,30 @@ namespace PraProjekt
         //prati podatke u kojem je tabu
         private Button? lastPressedButton;
         public User OvajUser { get; set; }
+
+        private const string Kolegiji_Path = "kolegiji.txt";
+        private const string Obavijesti_Path = "obavijesti.txt";
+
+        List<Obavijest> obavijesti = new List<Obavijest>();
+        List<Kolegij> kolegiji = new List<Kolegij>();
         public MainWindow()
         {
             LoginUsera();
             InitializeComponent();
+            LoadObavijestiData();
+            LoadKolegijiData();
             CheckIfAdmin();
             DrawUser();
+        }
+
+        private void LoadKolegijiData()
+        {
+            //loadaj kolegiji iz datoteke
+        }
+
+        private void LoadObavijestiData()
+        {
+            //loadaj obavijesti iz datoteke
         }
 
         private void DrawUser()
@@ -53,6 +71,91 @@ namespace PraProjekt
             {
                 this.Close();
             }
+        }
+
+        private void BtnObavijest_click(object sender, RoutedEventArgs e)
+        {
+            Button but = sender as Button;
+            if (IsActiveTab(but)) 
+            {
+                return;
+            }
+            lastPressedButton = but;
+
+            ClearSpace();
+            PutContentOnScreen(obavijesti);
+        }
+
+        private void BtnPredmeti_click(object sender, RoutedEventArgs e)
+        {
+            Button but = sender as Button;
+            if (IsActiveTab(but))
+            {
+                return;
+            }
+            lastPressedButton = but;
+
+            ClearSpace();
+            PutContentOnScreen(kolegiji);
+        }
+
+
+        private void PutContentOnScreen(List<Obavijest> obavijesti)
+        {
+            foreach (var obavijest in obavijesti)
+            {
+                MakeObavijest(obavijest);
+            }
+        }
+
+        private void PutContentOnScreen(List<Kolegij> kolegiji)
+        {
+            foreach (var kolegij in kolegiji)
+            {
+                MakeKolegij();
+            }
+        }
+
+        private void MakeObavijest(Obavijest obavijest)
+        {
+            //napravi user controlu obavijest i stavi je u stackpanel
+        }
+
+        private void MakeKolegij()
+        {
+            //napravi user controlu kolegij i stavi je u stackpanel
+        }
+
+        private void BtnDodaj_click(object sender, RoutedEventArgs e)
+        {
+            Button but = sender as Button;
+            if (IsActiveTab(but))
+            {
+                return;
+            }
+            lastPressedButton = but;
+            ClearSpace();
+        }
+        private void ClearSpace()
+        {
+            StackPanelContent.Children.Clear();
+        }
+        private bool IsActiveTab(Button? but)
+        {
+            if (lastPressedButton == but)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //save data iz liste kolegija u kolegiji.txt
+            //save data iz liste obavijesti u obavijesti.txt
         }
     }
 }
