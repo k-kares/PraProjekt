9fb59c53010aff2dc9a6a96f7b5529245e4f2abb
LovroPavlov <lovro.pavlov@racunarstvo.hr>
2023-09-01 19:29:40 +0200

    Korisnici te njihovo dodavanje i uređivanje

---
+++ PraProjekt/DodajKorisnika.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Utilities;
+
+namespace PraProjekt
+{
+    /// <summary>
+    /// Interaction logic for DodajKorisnika.xaml
+    /// </summary>
+    public partial class DodajKorisnika : Window
+    {
+        private User trenutniKorisnik = new User();
+        MainWindow MainW = new MainWindow();
+        public DodajKorisnika(MainWindow mw)
+        {
+            MainW = mw;
+            InitializeComponent();
+        }
+
+        private void btnDodaj_Click(object sender, RoutedEventArgs e)
+        {
+            String korisnik = $"{tbNazivKorisnika.Text}|{tbEmailKorisnika.Text}|{tbPasswordKorisnika.Text}|{cbAdminStatus.IsChecked}|{++MainW.lastKorisnikId}";
+            File.AppendAllText(konstante.Korisnici_Path, Environment.NewLine);
+            File.AppendAllText(konstante.Korisnici_Path, korisnik);
+            this.Close();
+        }
+    }
+}
---
+++ PraProjekt/KorisnikView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Utilities;
+
+namespace PraProjekt
+{
+    /// <summary>
+    /// Interaction logic for UserView.xaml
+    /// </summary>
+    public partial class KorisnikView : UserControl
+    {
+        User trenutniUser = new User();
+        public KorisnikView(User ovajUser)
+        {
+            trenutniUser=ovajUser;
+            InitializeComponent();
+            SetInfo();
+        }
+
+        public void SetInfo()
+        {
+            lblName.Content = trenutniUser.Name;
+            if (trenutniUser.IsAdmin)
+            {
+                lblAdminStatus.Content = "Administrator";
+            }
+            else
+            {
+                lblAdminStatus.Content = "Predavač";
+            }
+        }
+
+        private void UserControl_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            UrediKorisnika window = new UrediKorisnika(trenutniUser);
+            window.ShowDialog();
+        }
+    }
+}
